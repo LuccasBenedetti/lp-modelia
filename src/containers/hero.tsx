6cbd19e156815083b3f86c40006773ef7655dff3
Vinicius Carneiro Charleaux <vinicius.charleaux@gmail.com>
2024-09-07 11:42:05 -0300

    bg image

--- src/containers/hero.tsx
+++ src/containers/hero.tsx
@@ -1,11 +1,10 @@
-import heroBackground from "../assets/background-hero.png";
 import heroModel from "../assets/model-hero.png";
 
 export function Hero() {
   return (
-    <div className="background-image">
-      <img src={heroBackground} alt="" className="relative" />
-      <img src={heroModel} alt="" className="" />
+    <div className=" bg-[url('./background-hero.png')] bg-cover bg-no-repeat flex-col gap-40 flex items-center justify-center py-20">
+      <img src={heroModel} alt="" className="mt-32"/>
+      aqui vao as marcas
     </div>
   );
 }
