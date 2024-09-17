99aa141027c52b03f0717d09fbb5b7cef6055c39
viniCharleauxSoitic <vinicius.charleaux@soitic.com>
2024-09-17 09:58:33 -0300

    pagina preco inicio

--- src/app-routes.routes.tsx
+++ src/app-routes.routes.tsx
@@ -8,7 +8,7 @@ export const router = createBrowserRouter([
     element: <Home />,
   },
   {
-    path: '/about',
+    path: '/pricing',
     element: <Pricing />,
   },
 ])
--- src/pages/pricing.tsx
+++ src/pages/pricing.tsx
@@ -1,9 +1,70 @@
-import React from "react";
+import React, { useState } from "react";
 
 export const Pricing:React.FC = () => {
+
+    const [selectedOption, setSelectedOption] = useState<'standard' | 'custom'>('standard')
+
     return (
-        <div>
-            pricing
-        </div>
+        <div className="flex flex-col">
+            <header className="h-16 flex items-center justify-center gap-24">
+                <span>img</span>
+
+                <div className="flex gap-6 text-[#6C727F]">
+                    <a href="">AI E-commerce</a>
+                    <a href="">AI Tools</a>
+                    <a href="">Solutions</a>
+                    <a href="">Pricing</a>
+                    <a href="">Resources</a>
+                    <a href="">API</a>
+                </div>
+                <div className="flex gap-4">
+                    <button className="bg-transparent text-[#5D50FF] font-bold">Get in touch</button>
+                    <button className="bg-[#0F1729] text-white px-4 py-2 rounded-lg">log in / Sign up</button>
+                </div>
+            </header>
+            <main className="flex flex-col items-center justify-center bg-blue-300">
+                <span className="text-[64px] mt-28">CHOOSE YOUR PLAN</span>
+
+                <p className="mt-7 max-w-[740px] text-center text-[#384150]">With AI-powered technologies, Vmake makes it effortless for everyone to create
+                professional product photography, dedicated AI clothing models, and build their brands.</p>
+            
+
+                <div className="flex mt-6 bg-[#E5E7EB] rounded-lg border-[4px] border-[#E5E7EB]">
+                    <button onClick={() => setSelectedOption('standard')} className={`w-[220px] rounded-md h-9 ${selectedOption === 'standard' ? 'bg-white text-[#384150] ' : ''}`}>Standard</button>
+                    <button onClick={() => setSelectedOption('custom')} className={`w-[220px] rounded-md h-9 ${selectedOption === 'custom' ? 'bg-white text-[#384150] ' : ''} `}>Custom</button>
+                </div>
+
+                <div className="flex gap-6 mt-20">
+                    <div className="h-[640px] w-[360px] rounded-3xl bg-white p-8 flex flex-col items-start">
+                        <span className="text-2xl font-bold">Hobby</span>
+                        <span className="text-[#6C727F]">
+                            For Personal or Non-commercial Projects
+                        </span>
+
+                        <span className="mt-6 text-2xl font-bold">Free</span>
+
+                        <div className="flex text-[14px] max-w-[250px] text-left mx-auto flex-col mt-5 gap-3 items-start justify-center">
+                            <p>Sign up to get 5 free credits</p>
+                            <p> Unlimited free image/video processing
+                            and preview for non-generative features </p>
+                            <p> Unlimited free SD image and video
+                            preview downloads </p>
+                        </div>
+
+                        <button className="mt-auto h-11 w-full bg-[#F8FAFC] border border-[#E5E7EB] rounded-xl">Sign up for Free</button>
+
+                    </div>
+                    <div className="h-[640px] w-[360px] rounded-3xl bg-white p-8 flex flex-col items-start">
+
+                        pro
+                    </div>
+                    <div className="h-[640px] w-[360px] rounded-3xl bg-white p-8 flex flex-col items-start">
+
+                        flexible
+                    </div>
+                </div>
+
+            </main>
+        </div> 
     )
 }
